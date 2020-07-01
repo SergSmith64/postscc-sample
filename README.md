e1b4a2935a280942835e188d89b66bb4d461a15f
SergSmith64 <sergsmith2008@yandex.ru>
2020-07-01 08:20:01 +0400

    init: create project

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # postscc-sample
+
+1. npm init
+2. install gulp, gulp-postcss
---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+// const { src, dest, parallel, watch } = require('gulp');
+
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
